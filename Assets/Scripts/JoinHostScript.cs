b729a74b20bfc8c50f486d25eab033a5865ebc2b
casperjarmer <casper@jarmer.dk>
2025-08-18 16:49:49 +0200

    Maybe we can join each other now

---
+++ Assets/Scripts/JoinHostScript.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System;
+using System.Threading.Tasks;
+using Unity.Services.Core;
+using Unity.Services.Authentication;
+using Unity.Services.Relay;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Services.Relay.Models;
+
+public class JoinHostScript : MonoBehaviour
+{
+
+    public void Join(string joinCode)
+    {
+        StartClientWithRelay(joinCode, "udp");
+    }
+    public void Host()
+    {
+        StartHostWithRelay(16,"udp");
+
+    }
+    public async Task<string> StartHostWithRelay(int maxConnections, string connectionType)
+    {
+        await UnityServices.InitializeAsync();
+        if (!AuthenticationService.Instance.IsSignedIn)
+        {
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+        }
+        var allocation = await RelayService.Instance.CreateAllocationAsync(maxConnections);
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(AllocationUtils.ToRelayServerData(allocation, connectionType));
+        var joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+        Debug.Log(joinCode);
+        return NetworkManager.Singleton.StartHost() ? joinCode : null;
+    }
+    public async Task<bool> StartClientWithRelay(string joinCode, string connectionType)
+    {
+        await UnityServices.InitializeAsync();
+        if (!AuthenticationService.Instance.IsSignedIn)
+        {
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+        }
+
+        var allocation = await RelayService.Instance.JoinAllocationAsync(joinCode: joinCode);
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(AllocationUtils.ToRelayServerData(allocation, connectionType));
+        return !string.IsNullOrEmpty(joinCode) && NetworkManager.Singleton.StartClient();
+    }
+}
