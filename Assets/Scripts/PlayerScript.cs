3fdabfedf0dc610c9da55a824aae67382b5b8b98
casperjarmer <casper@jarmer.dk>
2025-08-18 17:42:18 +0200

    BallSpawner

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,43 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerScript : NetworkBehaviour
+{
+    public GameObject object1;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!IsOwner)
+        {
+            return;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (IsServer)
+            {
+                SpawnObjectServer();
+            }
+            else
+            {
+                SpawnObjectServerRpc();
+            }
+            
+        }
+    }
+    void SpawnObjectServer()
+    {
+        GameObject clone = Instantiate(object1, transform.position + new Vector3(0,3,0), Quaternion.identity);
+        clone.GetComponent<NetworkObject>().Spawn();
+    }
+    [ServerRpc]
+    void SpawnObjectServerRpc()
+    {
+        SpawnObjectServer();
+    }
+}
